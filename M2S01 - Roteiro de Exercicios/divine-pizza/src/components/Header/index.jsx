3e724f359eb5e4e8f087737a2efe82acb2b6b43d
Samuel Haro <samuel.hm@hotmail.com>
2022-10-07 13:25:59 -0300

    commit #07 - creating front-end

---
+++ M2S01 - Roteiro de Exercicios/divine-pizza/src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import { Container } from './styles'
+
+export const Header = () => {
+  return (
+    <Container>
+      <h1>Divine Pizza</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Checkout</li>
+        </ul>
+      </nav>
+    </Container>
+  )
+}
