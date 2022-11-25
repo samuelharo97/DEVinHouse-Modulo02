4097c5bb55b1d3bfd39814b2911bca650ea8772b
Samuel Haro <samuel.hm@hotmail.com>
2022-11-25 16:22:49 -0300

    adds postgres config w/ typeORM

--- M2S09 - Roteiro de Exercicios/e-commerce-api/src/app.module.ts
+++ M2S09 - Roteiro de Exercicios/e-commerce-api/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [CartModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    CartModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
